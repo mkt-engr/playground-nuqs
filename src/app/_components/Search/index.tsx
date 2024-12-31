de0e65e88ef66f3c5b133ad75a1604a51475d3ac
Makito Mori <66419373+mkt-engr@users.noreply.github.com>
2024-12-31 22:03:34 +0900

    .

--- src/app/_components/Search/index.tsx
+++ src/app/_components/Search/index.tsx
@@ -1,20 +1,26 @@
 "use client";
 
 import { useQueryState } from "nuqs";
-import { FormEvent, useCallback, useEffect, useRef } from "react";
+import { FormEvent, useCallback } from "react";
 import { useFocusRef } from "./hooks/useFocusRef";
+import { useRouter } from "next/navigation";
 
 export const Search = () => {
   const [query, setQuery] = useQueryState("q", { defaultValue: "" });
+  const router = useRouter();
 
-  const handleSubmit = useCallback((e: FormEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-  }, []);
+  const handleSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      router.refresh();
+    },
+    [router]
+  );
 
   const ref = useFocusRef();
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         value={query}
@@ -22,9 +28,7 @@ export const Search = () => {
         name="q"
         ref={ref}
       />
-      <button type="submit" onSubmit={handleSubmit}>
-        検索
-      </button>
+      <button type="submit">検索</button>
       <h1>Search Results for {query}</h1>
     </form>
   );
