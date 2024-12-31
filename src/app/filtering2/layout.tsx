193eb84773b63f0458c355a6fdc6d2deee1b3c7d
Makito Mori <66419373+mkt-engr@users.noreply.github.com>
2024-12-31 22:10:04 +0900

    .

--- src/app/filtering2/layout.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./layout.module.scss";
-
-export default function DemoLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <section className={styles.demoWrapper}>{children}</section>;
-}
--- src/app/filtering2/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Search } from "../_components/Search";
-
-export default async function Page() {
-  return (
-    <div>
-      <Search />
-    </div>
-  );
-}
